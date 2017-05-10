6287faccdf5281a129e41436a5c97ac6fbbe8112
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-05-10 09:56:44 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Requires the composer autoloader
+ * @see https://getcomposer.org/doc/01-basic-usage.md#autoloading
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/**
+ * Loads the WordPress Environment and Template
+ */
+require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
